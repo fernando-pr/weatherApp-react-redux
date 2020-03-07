2b34f5c417ebb46db3a3de9772851e42565e98af
fernando-pr <fernando321987@gmail.com>
2020-03-07 12:03:47 +0100

    muestra informacion extendida con datos no reales

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ForecastItem from './ForecastItem';
 import './styles.css';
 
+const days = [
+    'lunes',
+    'martes',
+    'miercoles',
+    'jueves',
+    'viernes',
+];
+
+const data = {
+    temperature: 10,
+    humidity: 30,
+    weatherState: "sun",
+    wind: 'normal wind',
+};
 
 class ForecastExtended extends Component {
+
+    renderForecastItemDays(){
+        return days.map(day => 
+            (<ForecastItem weekDay={day} hour={10} data={data}>
+
+            </ForecastItem>));
+    }
+
     render() {
 
         const {city} = this.props;
@@ -11,6 +34,7 @@ class ForecastExtended extends Component {
         return (
         <div>
             <h2 className='forecast-title'>Pronóstico extendido para {city}</h2>
+            {this.renderForecastItemDays()}
         </div>
         );
     }
